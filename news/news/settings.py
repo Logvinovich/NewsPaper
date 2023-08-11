5e3841ecffbefcb29d7fdf14314554884b2e4904
Logvinovich <vilmoja@mail.ru>
2023-08-11 14:51:11 +0900

    dfdf

--- news/news/settings.py
+++ news/news/settings.py
@@ -159,10 +159,15 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ACCOUNT_FORMS = {'signup': 'sign.models.BasicSignupForm'}
 
-EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
-EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'demon.k1ller'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'xsP-8Y9-DSk-ZnK'  # пароль от почты
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 
-SERVER_EMAIL = 'demon.k1ller@yandex.ru'
+EMAIL_HOST_USER = 'demon.k1ller@yandex.ru'
+EMAIL_HOST_PASSWORD = 'qgfpcftnbjkzvozv'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
+
